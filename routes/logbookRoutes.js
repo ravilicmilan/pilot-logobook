559f97ab7b8ef824cf01210d6c1390f161ccc826
Milan Ravilic <ravilicmilan@gmail.com>
2025-09-24 22:56:23 +0200

    protect all logbook routes

--- routes/logbookRoutes.js
+++ routes/logbookRoutes.js
@@ -1,18 +1,17 @@
-import express from "express";
+import express from 'express';
 import {
   addNewEntry,
   getEntireLogBook,
   modifyEntry,
-  removeEntry
-} from "../controllers/logbookController.js";
+  removeEntry,
+} from '../controllers/logbookController.js';
 import { auth } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.post("/logbook", addNewEntry);
-router.get("/logbook", auth, getEntireLogBook);
-router.put("/logbook/:id", modifyEntry);
-router.delete("/logbook/:id", removeEntry);
-
+router.post('/logbook', auth, addNewEntry);
+router.get('/logbook', auth, getEntireLogBook);
+router.put('/logbook/:id', auth, modifyEntry);
+router.delete('/logbook/:id', auth, removeEntry);
 
 export default router;
