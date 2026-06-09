e20724ef591237af02e77760f011415d751a635d
Milan Ravilic <ravilicmilan@gmail.com>
2026-06-09 17:11:18 +0200

    add order by in query

--- services/logbookService.js
+++ services/logbookService.js
@@ -27,7 +27,7 @@ export const createNewRow = async (logdata) => {
 
 export const getAllLogs = async () => {
   try {
-    const result = await sql`SELECT * FROM logbook`;
+    const result = await sql`SELECT * FROM logbook ORDER BY date ASC`;
     return result;
   } catch (err) {
     throw new Error(err.message);
